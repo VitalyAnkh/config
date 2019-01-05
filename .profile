c379be8122a7792a565e42640718faaf8417a95b
VitalyR <vitalyankh@gmail.com>
2019-01-05 20:54:19 +0800

    Initial commit

---
+++ .profile
@@ -0,0 +1,14 @@
+export EDITOR=/usr/bin/vim
+export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
+
+
+
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS="@im=fcitx"
+export PATH=$HOME/.cargo/bin:$HOME/go/bin:$PATH
+export PATH=$HOME/anaconda3/bin:$PATH
+# export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/projects/dart/flutter/bin:$PATH
+export LGOPATH=$HOME/projects/go
+export GOPATH=$HOME/projects/go
---
+++ update-all.sh
@@ -0,0 +1,26 @@
+echo "Copy config files..."
+cp ~/.zshrc ~/.config/
+cp ~/.profile ~/.config/
+cp ~/.bashrc ~/.config/ 
+echo "Exchange CapsLock and Ctrl in X11..."
+setxkbmap -option "ctrl:swapcaps"
+echo "modprobe nvidia..."
+sudo modprobe nvidia
+echo "Navigat GPU status..."
+nvidia-smi
+echo "Update flutter..."
+flutter upgrade
+echo "Update rust..."
+rustup update
+echo "Update cargo components..."
+cargo install-update -a
+echo "Update anaconda libraries..."
+conda update --all -y
+echo "Update haskell stack libraries..."
+stack update
+echo "Update OCaml libraries..."
+opam update 
+opam upgrade -y
+echo "Update system packages..."
+yay
+echo "Done!"
