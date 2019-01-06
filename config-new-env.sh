bcfad865bb3b86fe6806c1ceabb5a7c1c37a28c1
VitalyR <vitalyankh@gmail.com>
2019-01-06 16:48:00 +0800

    Update script

--- config-new-env.sh
+++ config-new-env.sh
@@ -2,3 +2,15 @@ echo "Copy config files..."
 cp ~/projects/config/.bashrc ~/
 cp ~/projects/config/.zshrc ~/
 cp ~/projects/config/.profile ~/
+
+echo "Change shell to zsh"
+chsh -s /bin/zsh 
+
+echo "Configure directory jump..."
+mkdir /home/vitalyr/projects
+mkdir /home/vitalyr/projects/shell 
+git clone git@github.com:rupa/z.git
+./home/projects/shell/z/z.sh 
+
+echo "Configure rust environment..."
+curl https://sh.rustup.rs -sSf | sh
