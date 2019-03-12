2872f97e1e56130c13bf308e299120c608e9fd75
VitalyR <vitalyankh@gmail.com>
2019-03-12 17:20:11 +0800

    enable conda update

--- update-all.sh
+++ update-all.sh
@@ -1,6 +1,7 @@
-# TODO: use ansible to update
+# todo: use ansible to update
 # todo: update haskell ide engine
-# todo: clean build after building successfully
+# todo: clean build cache after building successfully
+echo "***************************************************************"
 echo "Copy config files..."
 cp ~/.zshrc ~/.config/
 cp ~/.profile ~/.config/
@@ -8,11 +9,14 @@ cp ~/.bashrc ~/.config/
 cp ~/.bashrc ~/projects/config
 cp ~/.zshrc ~/projects/config
 cp ~/.profile ~/projects/config
+echo "***************************************************************"
 echo "Update hosts files..."
 cd ~/projects/web/hosts/
 git pull
+echo "***************************************************************"
 echo "Copy hosts files..."
 sudo cp hosts-files/* /etc/
+echo "***************************************************************"
 echo "Add hosts to support coursera..."
 sudo echo "
 52.84.246.90    d3c33hcgiwev3.cloudfront.net
@@ -25,44 +29,60 @@ sudo echo "
 52.84.246.90    d3c33hcgiwev3.cloudfront.net
 52.84.246.227    d3c33hcgiwev3.cloudfront.net
 " >>/etc/hosts
+echo "***************************************************************"
 echo "Exchange CapsLock and Ctrl in X11..."
 setxkbmap -option "ctrl:swapcaps"
+echo "***************************************************************"
 echo "modprobe bluetooth..."
 modprobe bluetooth
+echo "***************************************************************"
 echo "modprobe nvidia..."
 sudo modprobe nvidia && sudo modprobe nvidia-modset && sudo modprobe nvidia-uvm
+echo "***************************************************************"
 echo "Update npm..."
 npm install npm@latest -g
+echo "***************************************************************"
 echo "Update rbenv..."
 cd ~/.rbenv
 git pull
 echo "Update ruby build..."
 cd "$(rbenv root)"/plugins/ruby-build && git pull
+echo "***************************************************************"
 echo "Navigat GPU status..."
 nvidia-smi
+echo "***************************************************************"
 echo "Update haskell ide engine..."
 cd ~/projects/haskell/haskell-ide-engine && git pull && make hie-8.6.3 && make build-doc-8.6.3
+echo "***************************************************************"
 echo "Update flutter..."
 flutter upgrade
+echo "***************************************************************"
 echo "Update rust..."
 rustup update
+echo "***************************************************************"
 echo "Update cargo components..."
 cargo install-update -a
+echo "***************************************************************"
 echo "Update rust analyzer..."
 cd ~/projects/rust/rust-analyzer
 git checkout -- Cargo.lock
 git pull
 cargo install-code
-rustup component add rust-src
-# echo "Update anaconda libraries..."
-# conda update --all -y
+echo "***************************************************************"
+echo "Update anaconda libraries..."
+conda update --all -y
+echo "***************************************************************"
 echo "Update servo..."
 cd ~/projects/rust/servo
 git pull && ./mach build --release
+echo "***************************************************************"
 echo "Update haskell stack libraries..."
 stack update
+echo "***************************************************************"
 echo "Update OCaml libraries..."
 opam update && opam upgrade -y
+echo "***************************************************************"
 echo "Update system packages..."
 yay
+echo "***************************************************************"
 echo "Done!"
