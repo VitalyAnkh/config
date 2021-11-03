39a041912dc1bc01054ad2aba5a106fabd99e7b2
vitalyr <vitalyankh@gmail.com>
2021-11-04 03:31:41 +0800

    clean emacs more

--- trash-emacs-cache.sh
+++ trash-emacs-cache.sh
@@ -1,4 +1,3 @@
-trash-put $SDK_DIR/config/emacs/emacs.d-doom/eln-cache
-trash-put $SDK_DIR/config/emacs/emacs.d-doom/.local/cache/eln
-trash-put $SDK_DIR/config/emacs/emacs.d-doom/.local/straight/build-28.0.50-cache.el
-trash-put $SDK_DIR/config/emacs/emacs.d-doom/.local/straight/build-28.0.50
+trash-put $HOME/.config/emacs/emacs.d-doom/eln-cache
+trash-put $HOME/.config/emacs/emacs.d-doom/.local/cache/eln
+trash-put $HOME/.config/emacs/emacs.d-doom/.local/straight/build-*.el
