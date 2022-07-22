a3b73a422824f170526bcd6cbb9952a51389b264
vitalyr <vitalyankh@gmail.com>
2022-07-22 14:56:31 +0800

    try to fix agda-mode load-path problem

--- doom/init.el
+++ doom/init.el
@@ -105,7 +105,7 @@
        ;;taskrunner                 ; taskrunner for all your projects
        ;;terraform                  ; infrastructure as code
        ;;tmux                       ; an API for interacting with tmux
-       tree-sitter                    ; syntax and parsing, sitting in a tree...
+       tree-sitter                  ; syntax and parsing, sitting in a tree...
        upload                       ; map local to remote projects via ssh/ftp
 
        :os
@@ -173,7 +173,7 @@
        ;;qt                         ; the 'cutest' gui framework ever
        (racket +lsp +xp)            ; a DSL for DSLs
        raku                         ; the artist formerly known as perl6
-       rest                       ; Emacs as a REST client
+       rest                         ; Emacs as a REST client
        ;;rst                        ; ReST in peace
        (ruby +rails)                ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp)                  ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
