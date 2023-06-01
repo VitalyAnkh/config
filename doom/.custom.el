fb20d803e55eb6bf338cf0489c979467d4837a9c
VitalyR <vitalyankh@gmail.com>
2023-06-01 11:47:06 +0800

    update .gitignore, make git-ignore except some subdirectories work

---
+++ doom/.custom.el
@@ -0,0 +1,45 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values '((package-lint-main-file . "haskell-mode-pkg.el")))
+ '(org-safe-remote-resources
+   '("\\`https://gitee\\.com/Vitaly/img/raw/master/images/habitica\\.png\\'" "\\`https://gitee\\.com/Vitaly/img/raw/master/anime\\.jpg\\'"))
+ '(safe-local-variable-values
+   '((whitespace-line-column . 79)
+     (package-lint-main-file . "haskell-mode-pkg.el")
+     (diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")
+     (eval ignore-errors
+      (require 'whitespace)
+      (whitespace-mode 1))
+     (whitespace-style face indentation)
+     (eval progn
+      (c-set-offset 'case-label '0)
+      (c-set-offset 'innamespace '0)
+      (c-set-offset 'inline-open '0)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-modified ((t (:foreground "orange"))))
+ '(markdown-header-face-1 ((t (:height 1.25 :inherit markdown-header-face))))
+ '(markdown-header-face-2 ((t (:height 1.15 :inherit markdown-header-face))))
+ '(markdown-header-face-3 ((t (:height 1.08 :inherit markdown-header-face))))
+ '(markdown-header-face-4 ((t (:height 1.0 :inherit markdown-header-face))))
+ '(markdown-header-face-5 ((t (:height 0.9 :inherit markdown-header-face))))
+ '(markdown-header-face-6 ((t (:height 0.75 :inherit markdown-header-face))))
+ '(org-document-title ((t (:height 1.3))))
+ '(org-modern-statistics ((t (:inherit org-checkbox-statistics-todo))))
+ '(org-quote ((t (:font "Linux Libertine" :height 1.05))))
+ '(org-table ((t (:font "Sarasa Mono SC" :height 1.05))))
+ '(outline-1 ((t (:height 1.25))))
+ '(outline-2 ((t (:height 1.15))))
+ '(outline-3 ((t (:height 1.12))))
+ '(outline-4 ((t (:height 1.09))))
+ '(outline-5 ((t (:height 1.06))))
+ '(outline-6 ((t (:height 1.03))))
+ '(outline-8 ((t (:height 1.01))))
+ '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
